517b9f4a5e4529efa1b38e775ed049cd30dab815
Deepak Raj Regmi <deepakreg360@gmail.com>
2026-04-21 16:11:12 +0545

    Render deployment setup

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0_e9ox9p*$+9064$e=zlvj*q&b4l-i#-$26&pjim73v#j=ruvq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
